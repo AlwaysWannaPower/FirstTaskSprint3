33ddd9f3ee8aeb52e7e46686189d76b83b6c729b
AlwaysWannaPower <augustgants@gmail.com>
2022-11-25 18:29:26 +0300

    добавил кнопку и лейбол, пробросил связи, написал логику, небольшой рефакторинг

--- FirstTask3/ViewController.swift
+++ FirstTask3/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Subviews
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    
+    // MARK: - Instance Properties
+    /// Эта переменная-счётчик
+    private var count: Int = 0 {
+        didSet{
+            /// При каждом обновлении значения переменной - обновляем текст в лэйбле
+            updateCountLabel()
+        }
+    }
+    
+    // MARK: - Instance Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func ButtonTapedCount(_ sender: Any) {
+        count+=1
+    }
+    
+    // MARK: - Private Methods
+    private func updateCountLabel() {
+        countLabel.text = "Значение счётчика: \(String(count))"
+    }
 }
 
